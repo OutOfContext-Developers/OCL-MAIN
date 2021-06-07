f120c4c3a1a2f66d65eec573c4d781fcb8e42686
Akshat1Nar <akshat.dixit71@gmail.com>
2021-06-07 13:59:13 +0530

    Updated script to install the software

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
-cd lib/OCL-VideoProcessingLibrary
-sudo ./run.sh
+DEPENDENCIES="libavformat-dev libavcodec-dev libavutil-dev libavdevice-dev libavfilter-dev libswresample-dev libswscale-dev "
 
-cd ../../
+# Installing required packages
+# for package in $DEPENDENCIES; do
+#   if [ $(dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -c "ok installed") -eq 0 ];
+#   then
+#     apt-get install $package;
+#   fi
+# done
 
 rm -rf build/
 mkdir build
